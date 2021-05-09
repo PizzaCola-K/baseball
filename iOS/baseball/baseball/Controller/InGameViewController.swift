fb41c19d41ff9f2d762a73260196366ba267416d
jhpark-steve <park9368@naver.com>
2021-05-09 22:55:29 +0900

    feat: lottie 기본

--- iOS/baseball/baseball/Controller/InGameViewController.swift
+++ iOS/baseball/baseball/Controller/InGameViewController.swift
@@ -6,18 +6,39 @@
 //
 
 import UIKit
+import Lottie
 
 class InGameViewController: UIViewController {
     
     @IBOutlet weak var pitchingHistoryTableView: UITableView!
     @IBOutlet weak var inningInfoView: InningInfoView!
     
+    let animationView: AnimationView = {
+        let animationView = AnimationView(name: "data")
+        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animationView.tag = 100
+        animationView.contentMode = .scaleAspectFit
+        return animationView
+    }()
+    
     private var dataSource = PitchingHistoryDataSource()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        //        super.viewDidLoad()
         dataSource.setupDataSource(tableView: pitchingHistoryTableView)
         self.inningInfoView.applyBallCount(strike: 1, ball: 2, out: 1)
+    }
+    
+    @IBAction func Pitch(_ sender: Any) {
+        self.view.addSubview(animationView)
+        animationView.play { [weak self] _ in
+            self?.removeAnimationView()
+        }
+    }
+    private func removeAnimationView() {
+        if let tag = self.view.viewWithTag(100) {
+            tag.removeFromSuperview()
+        }
     }
 }
 
