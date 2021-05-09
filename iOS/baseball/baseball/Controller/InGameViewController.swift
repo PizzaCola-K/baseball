ad4be5902ff00e697ce94691fcb3e9eae833e713
BMO <b38238496@gmail.com>
2021-05-09 22:57:50 +0900

    feat: Create FieldView

--- iOS/baseball/baseball/Controller/InGameViewController.swift
+++ iOS/baseball/baseball/Controller/InGameViewController.swift
@@ -11,13 +11,15 @@ class InGameViewController: UIViewController {
     
     @IBOutlet weak var pitchingHistoryTableView: UITableView!
     @IBOutlet weak var inningInfoView: InningInfoView!
+    @IBOutlet weak var fieldView: FieldView!
     
     private var dataSource = PitchingHistoryDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dataSource.setupDataSource(tableView: pitchingHistoryTableView)
-        self.inningInfoView.applyBallCount(strike: 1, ball: 2, out: 1)
+        self.inningInfoView.applyBallCount(strike: 2, ball: 2, out: 1)
+        self.inningInfoView.applyBallCount(strike: 0, ball: 0, out: 2)
     }
     
 }
